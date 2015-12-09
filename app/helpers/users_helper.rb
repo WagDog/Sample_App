512f1eeff028450c98c792e4d5975c4e3e059fac
Paul Wagstaff <paulwagstaff66@gmail.com>
2015-12-09 15:51:27 +0000

    After changes to double quoted strings to single quotes for non-interpolated strings. 2015-12-09

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
 
   # Return the Gravatar for the given user from http://gravatar.com position 8375
+  # Gravatar Login P - tr34cl3t4rt
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png" # This works
