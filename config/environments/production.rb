a051ca1f045089878c5485ccbe4391f94fee407c
Paul Wagstaff <paulwagstaff66@gmail.com>
2015-12-10 10:17:32 +0000

    Added PostGre gem to the production group. 2015-12-09

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
+  # NOTE - The lines below are from http://stackoverflow.com/questions/16271696/cant-get-css-working-on-heroku-using-rails-4-with-bootstrap-sass-gem
+  # This refers to Bootstrap not working on Heroku
+  config.serve_static_files = true
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
